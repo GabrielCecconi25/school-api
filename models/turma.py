d3f6fefecb967c5c81e39b5ecb74e91164797123
Gabriel T. Cecconi <gabriel.cecconi25@gmail.com>
2025-03-06 15:59:30 -0300

    feature/model turma

---
+++ models/turma.py
@@ -0,0 +1,19 @@
+from database import db
+from models.professor import Professor
+
+class Turma(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    descricao = db.Column(db.String(100), nullable=False)
+    ativo = db.Column(db.Boolean, nullable=False)
+    professor_id = db.Coloum(db.Integer, db.ForeignKey('professor.id'))
+
+    # Relacionamento
+    professor = db.relationship('Professor', backref='turmas', nullable=True)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'descricao': self.descricao,
+            'ativo': self.ativo,
+            'professor_id': self.professor_id
+        }
